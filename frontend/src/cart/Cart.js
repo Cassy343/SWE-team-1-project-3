33c65e1cb028e97dcbdd11cd6cc8bea010f26dcc
Tommy Bonina <kss5zj@virginia.edu>
2022-06-07 15:53:26 -0400

    added uid to cart

--- frontend/src/cart/Cart.js
+++ frontend/src/cart/Cart.js
@@ -1,17 +1,39 @@
 import Product from '../Components/Product';
 import Receipt from './Receipt'
 import {Stack} from '@mui/material'
-import { useState } from 'react';
 import './Cart.css';
-import { useEffect } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { SessionContext } from '../Context'
 
-const Cart = (props) => {
+const Cart = () => {
 
     const [products, setProducts] = useState([]);
 
+    const session = useContext(SessionContext);
+    console.log("session uid", session.uid);
+    const iansUID = 'PiO0LdBIBGOEpDvm2bt1H86G9Au2'
+    const url = "products?user=" + iansUID;
+
     useEffect(() => {
-      
-    }, [])
+      fetch(url)
+      .then((res) => res.json())
+      .then((text) => {
+          const newProducts = text.result.map(p => {
+              return {
+                  id: p.id,
+                  name: p.name,
+                  price: p.price,
+                  description: p.description,
+                  image: p.image,
+                  date: p.date,
+              };
+          })
+  
+          setProducts(newProducts);
+          console.log("products", products);
+      })
+      .catch((err) => console.log(err))
+  }, [products, url])
     
 
     return (
